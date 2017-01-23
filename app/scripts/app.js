663160062c9eca4ecb7a6c515fa17fb5fe223acd
Mihaita Ivli <mihaitaivli@gmail.com>
2017-01-23 03:42:36 +0000

    Add functionality to the toggle button

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,6 +2,7 @@
 angular.module('confusionApp', []).controller('MenuController', ['$scope', function($scope) {
     $scope.tab = 1;
     $scope.filtText = '';
+    $scope.showDetails = false;
 
     var dishes = [
         {
@@ -67,4 +68,9 @@ angular.module('confusionApp', []).controller('MenuController', ['$scope', funct
     $scope.isSelected = function (checkTab) {
         return ($scope.tab === checkTab);
     };
+
+    $scope.toggleDetails = function(){
+        $scope.showDetails = !$scope.showDetails;
+    };
+
 }]);
